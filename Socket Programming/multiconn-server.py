9d7791a25f8b446c41781f68c4a4e2e1bcddbdaf
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-25 21:19:43 +0000

    Update multiconn-server.py

--- Socket Programming/multiconn-server.py
+++ Socket Programming/multiconn-server.py
@@ -3,6 +3,33 @@ import socket
 import selectors
 import types
 
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+
 sel = selectors.DefaultSelector()
 
 host, port = sys.argv[1], int(sys.argv[2])
